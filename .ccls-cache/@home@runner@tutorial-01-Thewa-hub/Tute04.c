7d507bd09200e75d5b06384921e922de6e27f8a0
Thewan Damnidu <117763807+Thewa-hub@users.noreply.github.com>
2023-03-17 00:05:03 +0000

    Done exercise 04

--- .ccls-cache/@home@runner@tutorial-01-Thewa-hub/Tute04.c
+++ .ccls-cache/@home@runner@tutorial-01-Thewa-hub/Tute04.c
@@ -5,6 +5,10 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+//define the functions
+int minimum(no1, no2);
+int  maximum(no1, no2);
+int multiply(no1, no2);
 
 int main() {
    int no1, no2;
@@ -16,4 +20,23 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}
+int minimum(no1, no2){
+  if(no1>no2){
+    return no2;
+  }
+  else if (no1<no2){
+    return no1;
+  }
+}
+int  maximum(no1, no2){
+   if(no1>no2){
+    return no1;
+  }
+  else if (no1<no2){
+    return no2;
+  }
+}
+int multiply(no1, no2){
+  return no1*no2;
 }
